b3dd1fd32623df2a709c96de162d93b123b7a426
Neal <83289930+nealioa@users.noreply.github.com>
2023-02-25 13:29:37 +0000

    Enable CORS

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const router = require('./config/routes');
 
 const app = express();
 const port = 3000;
 
+app.use(cors()); // enable all CORS requests
 app.use(express.json());
 
 // Add all routes to app
